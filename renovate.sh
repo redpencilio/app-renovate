92c31d710f52b94a0cc697225a91284b8cf50d86
abeforgit <arnebertrand@gmail.com>
2023-06-20 18:54:54 +0200

    fix: add shebang line

--- renovate.sh
+++ renovate.sh
@@ -1,2 +1,4 @@
+#! /usr/bin/env sh
+
 RENOVATE_TOKEN=$(./refresh-token.sh)
 docker run --rm -v "$PWD/config/renovate/renovate.js:/usr/src/app/config.js" -e RENOVATE_TOKEN="$RENOVATE_TOKEN" renovate/renovate:slim
